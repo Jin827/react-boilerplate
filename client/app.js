583b36b26bc1e11e19fdb35d3c2871c2ab2abb52
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-14 01:26:06 +0900

    React Hot Reloading

--- client/app.js
+++ client/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './src/components/counter';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-  <h2 className="react-container">Hello, from React!</h2>,
-  document.getElementById('react-root')
-)
+function render(Component) {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('react-root')
+  )
+}
+
+render(Counter)
+
+if (module.hot) {
+  module.hot.accept('./src/components/counter', () => {
+    const NewCounter = require('./src/components/counter').default
+    render(NewCounter)
+  })
+}
