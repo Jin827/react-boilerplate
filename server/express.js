24237991d3461c5f24cb22bf942ea0dafc0ac66c
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-14 04:26:29 +0900

    Production Ready CSS

--- server/express.js
+++ server/express.js
@@ -1,24 +1,26 @@
 import express from 'express';
 import path from 'path';
-
 const server = express();
 
-const webpack = require('webpack');
-const config = require('../webpack.dev');
-const compiler = webpack(config);
-const webpackDevMiddleware = require('webpack-dev-middleware')(
-  compiler,
-  config.devServer
-);
-const webapckHotMiddleware = require('webpack-hot-middleware')(compiler);
+const isProd = process.env.NODE.ENV === "production";
+if (!isProd) {
+  const webpack = require('webpack');
+  const config = require('../webpack.dev');
+  const compiler = webpack(config);
 
-server.use(webpackDevMiddleware);
-server.use(webapckHotMiddleware);
+  const webpackDevMiddleware = require('webpack-dev-middleware')(
+    compiler,
+    config.devServer
+  );
+  const webapckHotMiddleware = require('webpack-hot-middleware')(compiler);
 
+  server.use(webpackDevMiddleware);
+  server.use(webapckHotMiddleware);
+}
 const staticMiddleware = express.static('dist');
-
 server.use(staticMiddleware);
 
-server.listen(9007, () => {
-  console.log('Sever is listening');
+const PORT = process.env.PORT || 9007
+server.listen(PORT, () => {
+  console.log(`Sever is listening on ${PORT}`);
 })
---
+++ webpack.prod.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: ['./client/index.js']
+  },
+  mode: 'production',
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          attrs: ['img:src']
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/images/[name].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/svg/[name].[ext]',
+        },
+      },
+    ]
+  },
+  plugins: [
+    new OptimizeCssAssetsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash].css'
+    }),
+    new HTMLWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
