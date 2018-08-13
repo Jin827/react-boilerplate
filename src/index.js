01c6f4be0995a11f03d01e0ad8862ff5fd4daf3a
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-13 16:37:19 +0900

    ES17 Features Support Plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-alert('Hi');
+require('babel-runtime/regenerator');
 require('./styles/main.css');
 require('../public/index.html');
+
+var a = async(args) => {
+  const { a, b } = args
+  await console.log('Arrow Function!');
+  console.log('Done');
+}
+
+a({ a:1, b:2 });
--- webpack.dev.js
+++ webpack.dev.js
@@ -16,6 +16,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+        ],
+      },
+      {
         test: /\.css$/,
         use: [
           {
