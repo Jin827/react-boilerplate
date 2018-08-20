4cd4b9a39cae700093a510d1ac9d7f57fbe5fdbd
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-20 18:51:04 +0900

    Import only 'render' from 'react-dom'

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import Store from './Store';
@@ -10,15 +10,9 @@ require('./styles/main.scss');
 const root = document.createElement('div');
 document.body.appendChild(root);
 
-ReactDOM.render(
+render(
   <Provider store={Store}>
     <Routes />
   </Provider>,
   root,
 );
-
-// @flow
-const name: string = 'Lee';
-const age: number = 40;
-const somethig: any = [1, 2, 3];
-const users: string[] = ['John', 2];
