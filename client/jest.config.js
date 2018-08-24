157dac91f1fbcdcbfab20ee0de0267bb48b83f5e
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-25 00:17:12 +0900

    Jest Config Folder Name Change

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupTestFrameworkScriptFile: require.resolve(
-    '../test/setup-test-framework.js',
+    '../test.config/setup-test-framework.js',
   ),
   moduleNameMapper: {
     // module must come first
     // '\\.module\\.css$': 'identity-obj-proxy',
-    '\\.scss||css$': require.resolve('../test/style-mock.js'),
+    '\\.scss||css$': require.resolve('../test.config/style-mock.js'),
     // can also map files that are loaded by webpack with the file-loader
   },
   collectCoverageFrom: ['**/src/**/*.js'],
